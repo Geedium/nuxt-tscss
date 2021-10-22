fc09b34da4b453ded689b381ea857ee516ea7adb
Gediminas Dausynas <gediminas.dausynas@gmail.com>
2021-10-22 03:54:46 +0300

    New commit

--- src/module.ts
+++ src/module.ts
@@ -11,10 +11,10 @@ const tscss: Module<Options> = function (moduleOptions) {
   this.options.css.push(resolve(__dirname, "style.css"));
 
   if (!options.stylesOnly) {
-    this.addPlugin({ src: resolve(__dirname, "plugin") });
+    this.addPlugin({ src: resolve(__dirname, "plugin.js") });
   }
 };
 
 export default tscss;
 
-module.exports.meta = require("./package.json");
+module.exports.meta = require("../package.json");
