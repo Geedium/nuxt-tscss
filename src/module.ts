329af941da291e33a7fc10cf534e5d774a761eec
Gediminas Dausynas <gediminas.dausynas@gmail.com>
2021-10-31 01:13:46 +0300

    SSR

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nuxt/types";
 import { resolve } from "path";
 
-import { Styles } from "./types";
+import { DisplayType, Styles } from "./types";
 
 interface Options {}
 
@@ -22,26 +22,51 @@ const tscss: Module<Options> = function (moduleOptions) {
       binding: any
     ) => {
       const style = vnode.data.style || (vnode.data.style = {});
-
-      let computedClass: string[] = [];
+      const classList = vnode.data.class || (vnode.data.class = []);
 
       for (const [key, value] of Object.entries<any>(binding.value)) {
         switch (key as keyof Styles) {
           case "color":
             if (["primary", "secondary", "accent"].indexOf(value) !== -1) {
-              computedClass.push(`text-${value}`);
+              classList.push(`text-${value}`);
             } else {
               style.color = value;
             }
             break;
+          case "backgroundColor":
+            if (["primary", "secondary", "accent"].indexOf(value) !== -1) {
+              classList.push(`bg-${value}`);
+            } else {
+              style.backgroundColor = value;
+            }
+            break;
+          case "display":
+            switch (value as DisplayType) {
+              case "block":
+                classList.push("is-block");
+                break;
+              case "inline":
+                classList.push("is-inline");
+                break;
+              case "inlineBlock":
+                classList.push("is-inline-block");
+                break;
+              case "flex":
+                classList.push("is-flex");
+                break;
+              case "inlineFlex":
+                classList.push("is-inline-flex");
+                break;
+              case "grid":
+                classList.push("is-grid");
+                break;
+              case "table":
+                classList.push("is-table");
+                break;
+            }
+            break;
         }
       }
-
-      if (typeof vnode.data.class === "string") {
-        computedClass.push(vnode.data.class);
-      }
-      
-      vnode.data.class = computedClass;
     };
   }
 
